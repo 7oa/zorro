4c466265b7e604110f9c01be57e71c54f5d60028
Ann Semenova <semenova.7oa@yandex.ru>
2018-06-18 13:18:17 +0300

    header

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -2,5 +2,15 @@
 $(document).ready(function() {
     $('.js-menu').click(function () {
         $(this).toggleClass('open');
+        $('.js-dd').slideToggle();
     });
+
+    //клонируем элементы из мобильной в десктоп
+    $('.header-menu').clone().appendTo('.header-menu-desktop');
+    $('.header-auth').clone().appendTo('.header-desktop-r1__auth');
+    $('.header-call').clone().appendTo('.header-desktop-r1__call');
+    $('.header-fav').clone().appendTo('.header-line2__fav');
+    $('.header-backet').clone().appendTo('.header-line2__backet');
+    $('.header-tren').clone().appendTo('.header-tren-desktop');
+    $('.header-search').clone().appendTo('.header-search-desktop');
 });
