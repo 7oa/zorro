cf0745832f4307e4d28ed3a63ae9dc121c241c50
Ann Semenova <semenova.7oa@yandex.ru>
2018-06-18 17:49:32 +0300

    header

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -1,11 +1,16 @@
 // js jquery
+var isDesktop = window.matchMedia('(min-width: 1200px)').matches;
+$(window).on('load resize', function(){
+    isDesktop = window.matchMedia('(min-width: 1200px)').matches;
+});
 $(document).ready(function() {
+    //toggle моблильное меню
     $('.js-menu').click(function () {
         $(this).toggleClass('open');
         $('.js-dd').slideToggle();
     });
 
-    //клонируем элементы из мобильной в десктоп
+    //клонируем элементы из мобильной для десктопа
     $('.header-menu').clone().appendTo('.header-menu-desktop');
     $('.header-auth').clone().appendTo('.header-desktop-r1__auth');
     $('.header-call').clone().appendTo('.header-desktop-r1__call');
@@ -13,4 +18,33 @@ $(document).ready(function() {
     $('.header-backet').clone().appendTo('.header-line2__backet');
     $('.header-tren').clone().appendTo('.header-tren-desktop');
     $('.header-search').clone().appendTo('.header-search-desktop');
+
+
+
+    if (isDesktop) {
+        //закрепляем меню
+        $(window).scroll(function () {
+            var headerHeight = $('.header__r1').height() + $('.header__r2').height();
+            if ($(this).scrollTop() > headerHeight) {
+                $('.header').addClass('fixed');
+            } else {
+                $('.header').removeClass('fixed');
+            }
+        });
+
+        //покажем строку поиска для десктопа
+        $('.header-search').click(function(){
+            $('.header-search-line').fadeIn();
+        });
+    }
+});
+//скроем скроку поиска при клике вне элемента
+$(document).mouseup(function (e){
+    if (isDesktop) {
+        var div = $('.header-search-line');
+        if (!div.is(e.target)
+            && div.has(e.target).length === 0) {
+            div.fadeOut();
+        }
+    }
 });
